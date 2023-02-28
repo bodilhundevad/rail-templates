88c3779975438e786433e6a1b8daf1496eb8a5c6
Bodil Hundevad <bodil@hundevad.de>
2023-02-28 18:36:25 +0100

    Init basic.rb
    Rails template excluding LeWagon CSS and devise

---
+++ basic.rb
@@ -0,0 +1,148 @@
+run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
+
+# Gemfile
+########################################
+inject_into_file "Gemfile", before: "group :development, :test do" do
+  <<~RUBY
+    gem "autoprefixer-rails"
+    gem "font-awesome-sass", "~> 6.1"
+    gem "simple_form"
+  RUBY
+end
+
+inject_into_file "Gemfile", after: 'gem "debug", platforms: %i[ mri mingw x64_mingw ]' do
+<<-RUBY
+  gem "dotenv-rails"
+RUBY
+end
+
+gsub_file("Gemfile", '# gem "sassc-rails"', 'gem "sassc-rails"')
+
+# Assets
+########################################
+run "rm -rf app/assets/stylesheets"
+run "rm -rf vendor"
+run "mv app/assets/rails-stylesheets-master app/assets/stylesheets"
+
+inject_into_file "config/initializers/assets.rb", before: "# Precompile additional assets." do
+  <<~RUBY
+    Rails.application.config.assets.paths << Rails.root.join("node_modules")
+  RUBY
+end
+
+# Layout
+########################################
+
+gsub_file(
+  "app/views/layouts/application.html.erb",
+  '<meta name="viewport" content="width=device-width,initial-scale=1">',
+  '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
+)
+
+# Flashes
+########################################
+file "app/views/shared/_flashes.html.erb", <<~HTML
+  <% if notice %>
+    <div class="alert alert-info alert-dismissible fade show m-1" role="alert">
+      <%= notice %>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+      </button>
+    </div>
+  <% end %>
+  <% if alert %>
+    <div class="alert alert-warning alert-dismissible fade show m-1" role="alert">
+      <%= alert %>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+      </button>
+    </div>
+  <% end %>
+HTML
+
+inject_into_file "app/views/layouts/application.html.erb", after: "<body>" do
+  <<~HTML
+    <%= render "shared/flashes" %>
+  HTML
+end
+
+# Generators
+########################################
+generators = <<~RUBY
+  config.generators do |generate|
+    generate.assets false
+    generate.helper false
+    generate.test_framework :test_unit, fixture: false
+  end
+RUBY
+
+environment generators
+
+########################################
+# After bundle
+########################################
+after_bundle do
+  # Generators: db + simple form + pages controller
+  ########################################
+  rails_command "db:drop db:create db:migrate"
+  generate("simple_form:install", "--bootstrap")
+  generate(:controller, "pages", "home", "--skip-routes", "--no-test-framework")
+
+  # Routes
+  ########################################
+  route 'root to: "pages#home"'
+
+  # Gitignore
+  ########################################
+  append_file ".gitignore", <<~TXT
+    # Ignore .env file containing credentials.
+    .env*
+    # Ignore Mac and Linux file system files
+    *.swp
+    .DS_Store
+  TXT
+
+  # Application controller
+  ########################################
+  run "rm app/controllers/application_controller.rb"
+  file "app/controllers/application_controller.rb", <<~RUBY
+    class ApplicationController < ActionController::Base
+    end
+  RUBY
+
+  # migrate
+  ########################################
+  rails_command "db:migrate"
+ 
+  # Pages Controller
+  ########################################
+  run "rm app/controllers/pages_controller.rb"
+  file "app/controllers/pages_controller.rb", <<~RUBY
+    class PagesController < ApplicationController
+    end
+  RUBY
+
+  # Environments
+  ########################################
+  environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: "development"
+  environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: "production"
+
+  # Yarn
+  ########################################
+  run "yarn add bootstrap @popperjs/core"
+  append_file "app/javascript/application.js", <<~JS
+    import "bootstrap"
+  JS
+
+  # Heroku
+  ########################################
+  run "bundle lock --add-platform x86_64-linux"
+
+  # Dotenv
+  ########################################
+  run "touch '.env'"
+
+  # Git
+  ########################################
+  git :init
+  git add: "."
+  git commit: "-m 'Initial commit with basic template from https://github.com/bodilhundevad/rails-templates'"
+end
